f60ac59de718c038e557728327f4b743dae9ab5f
ajlopez <webmaster@ajlopez.com>
2017-08-20 12:49:15 -0300

    Constant expressions

---
+++ test/exprs.js
@@ -0,0 +1,31 @@
+
+var exprs = require('../lib/exprs.js');
+var types = require('../lib/types.js');
+
+exports['integer constant expression'] = function (test) {
+	var cons = exprs.constant(42, types.Int);
+	
+	test.ok(cons);
+	test.equal(typeof cons, 'object');
+	test.strictEqual(cons.evaluate(), 42);
+	test.strictEqual(cons.type(), types.Int);
+}
+
+exports['float constant expression'] = function (test) {
+	var cons = exprs.constant(3.14159, types.Float);
+	
+	test.ok(cons);
+	test.equal(typeof cons, 'object');
+	test.strictEqual(cons.evaluate(), 3.14159);
+	test.strictEqual(cons.type(), types.Float);
+}
+
+exports['string constant expression'] = function (test) {
+	var cons = exprs.constant('foo', types.String);
+	
+	test.ok(cons);
+	test.equal(typeof cons, 'object');
+	test.strictEqual(cons.evaluate(), 'foo');
+	test.strictEqual(cons.type(), types.String);
+}
+
