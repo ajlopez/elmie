666bf13374136307d9aa1d07cd163c075009172b
ajlopez <webmaster@ajlopez.com>
2017-08-20 13:14:29 -0300

    Type equal, functional type

--- test/types.js
+++ test/types.js
@@ -21,3 +21,23 @@ exports['number is a type'] = function (test) {
 	test.equal(typeof types.Number, 'object');
 }
 
+exports['functional type'] = function (test) {
+	var func = types.func(types.Int, types.Int);
+	
+	test.ok(func);
+	test.equal(typeof func, 'object');
+	test.strictEqual(func.input(), types.Int);
+	test.strictEqual(func.output(), types.Int);
+}
+
+exports['equals simple types'] = function (test) {
+	test.strictEqual(types.equal(types.Int, types.Int), true);
+	test.strictEqual(types.equal(types.Float, types.Float), true);
+	test.strictEqual(types.equal(types.String, types.String), true);
+	test.strictEqual(types.equal(types.Int, types.Float), false);
+	test.strictEqual(types.equal(types.Float, types.Int), false);
+	test.strictEqual(types.equal(types.Int, types.Number), false);
+	test.strictEqual(types.equal(types.Float, types.Number), false);
+	test.strictEqual(types.equal(types.Int, types.String), false);
+	test.strictEqual(types.equal(types.Float, types.String), false);
+}
