cae9d489bf6a1f6c02474ec8369538bc55cbdb2c
ajlopez <webmaster@ajlopez.com>
2017-08-20 13:19:02 -0300

    Functional types equal

--- test/types.js
+++ test/types.js
@@ -30,7 +30,7 @@ exports['functional type'] = function (test) {
 	test.strictEqual(func.output(), types.Int);
 }
 
-exports['equals simple types'] = function (test) {
+exports['simple types equal'] = function (test) {
 	test.strictEqual(types.equal(types.Int, types.Int), true);
 	test.strictEqual(types.equal(types.Float, types.Float), true);
 	test.strictEqual(types.equal(types.String, types.String), true);
@@ -41,3 +41,23 @@ exports['equals simple types'] = function (test) {
 	test.strictEqual(types.equal(types.Int, types.String), false);
 	test.strictEqual(types.equal(types.Float, types.String), false);
 }
+
+exports['functional types equal'] = function (test) {
+	var func1 = types.func(types.Int, types.Int);
+	var func2 = types.func(types.Int, types.Int);
+	var func3 = types.func(types.String, types.Int);
+	var func4 = types.func(types.String, types.func(types.Int, types.Float));
+	var func5 = types.func(types.String, types.func(types.Int, types.Float));
+	var func6 = types.func(types.String, types.func(types.Int, types.String));
+	
+	test.strictEqual(types.equal(func1, func2), true);
+	test.strictEqual(types.equal(func2, func1), true);
+	test.strictEqual(types.equal(func1, func3), false);
+	test.strictEqual(types.equal(func3, func1), false);
+	
+	test.strictEqual(types.equal(func4, func5), true);
+	test.strictEqual(types.equal(func5, func4), true);
+	test.strictEqual(types.equal(func4, func6), false);
+	test.strictEqual(types.equal(func6, func4), false);
+}
+
