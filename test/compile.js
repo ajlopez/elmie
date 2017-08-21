e8a427c14e8b2ca87815a621d1e24c8d67e26463
ajlopez <webmaster@ajlopez.com>
2017-08-21 15:07:57 -0300

    Additional compile tests

--- test/compile.js
+++ test/compile.js
@@ -15,3 +15,16 @@ exports['compile string'] = function (test) {
 	test.equal(result, '"foo";\n');
 }
 
+exports['compile dot expression'] = function (test) {
+	var result = elmie.compile('foo.bar');
+	
+	test.ok(result);
+	test.equal(result, 'foo.bar;\n');
+}
+
+exports['compile define expression'] = function (test) {
+	var result = elmie.compile('answer = 42');
+	
+	test.ok(result);
+	test.equal(result, 'var answer = 42;\n');
+}
