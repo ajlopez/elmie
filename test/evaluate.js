1ec62b2d7377efd612f6e5574b40c4680023b18e
ajlopez <webmaster@ajlopez.com>
2018-10-07 11:09:57 -0300

    Evaluate first tests

---
+++ test/evaluate.js
@@ -0,0 +1,14 @@
+
+var elmie = require('..');
+
+exports['evaluate integers'] = function (test) {
+	test.strictEqual(elmie.evaluate('42'), 42);
+	test.strictEqual(elmie.evaluate('0'), 0);
+}
+
+exports['evaluate strings'] = function (test) {
+	test.strictEqual(elmie.evaluate('"foo"'), 'foo');
+	test.strictEqual(elmie.evaluate('"bar"'), 'bar');
+	test.strictEqual(elmie.evaluate('""'), '');
+}
+
